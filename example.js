297a4b91dc7c1677d53a2dfbce697263ac055662
fsteff <fsteff@gmx.at>
2021-02-07 13:41:27 +0100

    example

--- example.js
+++ example.js
@@ -6,6 +6,10 @@ const remote = process.argv.length > 2 ? process.argv[2] : null
 simulator().then(start).catch(err => console.error(err))
 async function start({client, cleanup}) {
     const pubsub = new PubSub(client.network)
+    
+    console.log('joining the network...')
+    const key = await pubsub.join('test')
+    console.log('successfully joined the dht at topic hyper://' + key)
 
     if(remote) {
         await new Promise(resolve => {
@@ -17,9 +21,6 @@ async function start({client, cleanup}) {
             })
         })
     } else {
-        console.log('joining the network...')
-        const key = await pubsub.join('test')
-        console.log('successfully joined the dht at topic hyper://' + key)
         while (true) {
             pubsub.pub('test', Buffer.from('hello world!', 'utf-8'))
             //console.log('published msg')
